7d10d0e5be1171343fed89998515795efc80c246
D4YonSoundcloud <mattd4y@gmail.com>
2020-03-08 19:02:46 -0400

    7:02pm first commit game already done 3-8-20

---
+++ build/script.js
@@ -0,0 +1,126 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// list of words
+
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "one",
+  "eight",
+  "two"
+];
+
+//set difficulty in value to local storage, if none then default to medium
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+//set difficulty and select value
+difficultySelect.value =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+//random word
+let randomWord;
+
+//Init score
+let score = 0;
+
+//init time
+let time = 10;
+
+//focus on text on load
+text.focus();
+
+//start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+//add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+//update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    //end game
+    gameOver();
+  }
+}
+
+//game over and then show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p> Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+  endgameEl.style.display = "flex";
+}
+
+addWordToDOM();
+
+text.addEventListener("input", e => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+    //clear
+    e.target.value = "";
+    if (difficulty === "hard") {
+      time += 1;
+    } else if (difficulty === "medium") {
+      time += 3;
+    } else {
+      time += 5;
+    }
+    updateTime();
+  }
+});
+
+//settings btn click
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+//settings select
+settingsForm.addEventListener("change", e => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
